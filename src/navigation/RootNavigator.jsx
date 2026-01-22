99f78e4dff085ee8fd27235a2c838514c53476e3
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2026-01-22 20:46:51 +0200

    Extract info screen

--- src/navigation/RootNavigator.jsx
+++ src/navigation/RootNavigator.jsx
@@ -2,7 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import HomeNavigator from "./HomeNavigator";
 import CartScreen from "../screens/CartScreen";
-import { Text } from "react-native";
+import InfoScreen from "../screens/InfoScreen";
 
 export default function RootNavigator() {
     const Tabs = createBottomTabNavigator();
@@ -11,7 +11,7 @@ export default function RootNavigator() {
         <Tabs.Navigator>
             <Tabs.Screen name="HomeTab" component={HomeNavigator} />
             <Tabs.Screen name="Cart" component={CartScreen} />
-            <Tabs.Screen name="Info" component={() => <Text>Info Screen</Text>} />
+            <Tabs.Screen name="Info" component={InfoScreen} />
         </Tabs.Navigator>
     );
 }
